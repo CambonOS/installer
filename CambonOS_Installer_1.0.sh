4340b5b447134085ea286903214a6b05aef065a1
ManuCr19 <82413811+ManuCr19@users.noreply.github.com>
2021-04-12 22:21:55 +0200

    Add files via upload

---
+++ CambonOS_Installer_1.0.sh
@@ -0,0 +1,151 @@
+#!/bin/bash
+#
+SALIDA="/tmp/salida"
+clear
+echo -e "*******************************************************************************************************"
+echo -e "********************************* CAMBON OS INSTALLER *************************************************"
+echo -e "*******************************************************************************************************"
+
+echo -e "\n>>Iniciando instalacion"
+loadkeys es && ping -c 4 archlinux.org >$SALIDA 2>&1 || echo "NON HAY CONECXION A INTERNET"
+
+echo -e "\n>>Tipo de arranque?(uefi/bios) \c" && read GRUB
+echo -e "\n\n>>Listando discos\n" && lsblk
+echo -e "\n\n>>En que disco quieres instalar el sistema? \c" && read -e -i "/dev/sd" DISCO
+echo -e "\n\n>>Formato del disco?(mbr/gpt) \c" && read TDISCO
+echo -e "\n\n>>Nombre del equipo? \c" && read NOMBRE
+echo -e "\n\n>>Dominio?\ \c" && read DOMINIO
+echo -e "\n\n>>Procesador?(intel/amd) \c" && read CPU
+echo -e "\n\n>>Graficos?(nvidea/amd/vmware) \c" && read GPU
+echo -e "\n\n>>Entorno grafico?(terminal/gnome) \c" && read GDM
+echo -e "\n\n>>Contraseña del root? \c" && read -s PASS
+
+NULL=">/dev/null 2>&1"
+BOOT="$DISCO \b1"
+SWAP="$DISCO \b2"
+RAIZ="$DISCO \b3"
+HOME="$DISCO \b4"
+OUEFI="o\nn\np\n1\n\n+512M\nn\np\n2\n\n+4G\nn\np\n3\n\n+40G\nn\np\n4\n\n\nt\n1\nEF\nt\n2\n82\nt\n3\n83\nt\n4\n83\nw\n"
+OBIOS="o\nn\np\n1\n\n+512M\nn\np\n2\n\n+4G\nn\np\n3\n\n+40G\nn\np\n4\n\n\nt\n1\n83\nt\n2\n82\nt\n3\n83\nt\n4\n83\nw\n"
+GUEFI="o\nn\np\n1\n\n+512M\nn\np\n2\n\n+4G\nn\np\n3\n\n+40G\nn\np\n4\n\n\nt\n1\n1\nt\n2\n19\nt\n3\n23\nt\n4\n28\nw\n"
+GBIOS="o\nn\np\n1\n\n+512M\nn\np\n2\n\n+4G\nn\np\n3\n\n+40G\nn\np\n4\n\n\nt\n1\n4\nt\n2\n19\nt\n3\n23\nt\n4\n28\nw\n"
+
+clear
+
+echo -e "\n>>Actualizando reloj"
+timedatectl set-ntp true >>$SALIDA 2>&1 echo -e "\n>>Particionando disco"
+case $TDISCO in
+	gpt) case $GRUB in
+		uefi) echo -e $GUEFI | fdisk $DISCO >>$SALIDA 2>&1 ;; 
+		bios) echo -e $GBIOS | fdisk $DISCO >>$SALIDA 2>&1 ;;
+	esac ;;
+	mbr) case $GRUB in
+		uefi) echo -e $OUEFI | fdisk $DISCO >>$SALIDA 2>&1 ;;
+		bios) echo -e $OBIOS | fdisk $DISCO >>$SALIDA 2>&1 ;;
+	esac ;;
+esac
+
+echo -e "\n>>Formateando y montando sistemas de archivos"
+case $GRUB in
+	uefi) mkfs.ext4 $BOOT;;
+	bios) mkfs.fat -F32 $BOOT;;
+esac
+
+mkswap $SWAP >>$SALIDA 2>&1 
+mkfs.ext4 $RAIZ >>$SALIDA 2>&1 
+mkfs.ext4 $HOME >>$SALIDA 2>&1 
+swapon $SWAP >>$SALIDA 2>&1 
+mount $RAIZ /mnt >>$SALIDA 2>&1 
+mkdir /mnt/home >>$SALIDA 2>&1 
+mount $HOME /mnt/home >>$SALIDA 2>&1 
+mkdir /mnt/boot >>$SALIDA 2>&1 
+mount $BOOT /mnt/boot >>$SALIDA 2>&1 
+
+echo -e "\n>>Configurando pacman.conf"
+echo -e "\n[multilib]\nInclude = /etc/pacman.d/mirrorlist\n\nColor\nCheckSpace\nTotalDownload\nILoveCandy\n" >>/etc/pacman.conf
+
+echo -e "\n>>Seleccionando replicas"
+reflector -f 20 --save /etc/pacman.d/mirrorlist >>$SALIDA 2>&1 
+
+echo -e "\n>>Instalando base del sistema"
+pacstrap /mnt linux-zen linux-zen-headers linux-firmware base nano man man-db man-pages man-pages-es bash-completion neovim neofetch networkmanager grub $CPU-ucode git base-devel >>$SALIDA 2>&1 
+case $GRUB in
+	uefi) pacstrap /mnt efibootmgr;;
+	bios);;
+esac
+
+echo -e "\n>>Instalando drivers graficos"
+case $GPU in
+	amd) pacstrap /mnt xf86-video-vesa xf86-video-amdgpu lib32-mesa mesa vulkan-radeon lib32-vulkan-radeon vulkan-icd-loader lib32-vulkan-icd-loader;;
+	nvidia) pacstrap /mnt xf86-video-vesa nvidia lib32-nvidia-utils nvidia-utils nvidia-settings nvidea-dkms vulkan-icd-loader lib32-vulkan-icd-loader;;
+	intel) pacstrap /mnt xf86-video-vesa xf86-video-intel lib32-mesa mesa vulkan-intel lib32-vulkan-intel vulkan-icd-loader lib32-vulkan-icd-loader;;
+	vmware) pacstrap /mnt xf86-video-vesa xf86-video-vmware lib32-mesa mesa;;
+esac
+
+echo -e "\n>>Instalando entorno grafico seleccionado"
+case $GDM in
+	terminal) ;;
+	gnome) pacstrap /mnt gdm nautilus alacritty gedit gnome-calculator gnome-control-center gnome-tweak-tool ;;
+esac
+
+echo -e "\n>>Generando archivo fstab"
+genfstab -U /mnt >> /mnt/etc/fstab
+
+echo "
+	echo -e '\n>>Estableciendo zona horaria'
+	ln -sf /usr/share/zoneinfo/Europe/Madrid /etc/localtime
+	hwclock --systohc
+	
+	echo -e '\n>>Cambiando idioma del sistema'
+	echo 'es_ES.UTF-8 UTF-8\nen_US.UTF-8 UTF-8' >> /etc/locale.gen
+	locale-gen  >>$SALIDA 2>&1
+	echo -e 'LANG=es_ES.UTF-8\nLANGUAGE=es_ES.UTF-8\nLC_ALL=en_US.UTF-8' >/etc/locale.conf
+	echo -e 'KEYMAP=es' >/etc/vconsole.conf
+	
+	echo -e '\n>>Creando archivos host'
+	echo -e '$NOMBRE' >/etc/hostname
+	echo -e '127.0.0.1	localhost\n::1		localhost\n127.0.1.1	$DOMINIO $NOMBRE' >/etc/hosts
+	
+	echo -e '\n>>Configurando red'
+	systemctl enable NetworkManager.service >>/root/salida 2>&1
+	
+	echo -e '\n>>Configurando grub'
+	case $GRUB in
+		bios) grub-install --target=i386-pc $DISCO >>$SALIDA 2>&1 ;;
+		uefi) grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB ;;
+	esac
+	grub-mkconfig -o /boot/grub/grub.cfg >>$SALIDA 2>&1 	
+	echo -e '\n>>Activando entorno grafico'
+	case $GDM in
+		terminal) ;;
+		gnome) systemctl enable gdm.service ;;
+	esac
+
+	echo -e '\n>>Configurando root'
+	echo -e '$PASS\n$PASS' | passwd
+	
+	echo -e '\n>>Editando skel'
+	echo -e '\nneofetch' >/etc/skel/.bashrc
+
+	echo -e '\n>>Creando grupo sudo'
+	groupadd -g 513 sudo
+	echo '%sudo ALL=(ALL) NOPASSWD: ALL' >>/etc/sudoers
+	useradd -m -s /bin/bash -g sudo sysop
+	
+	echo -e '\n>>Instalando trizen'
+	echo '
+		cd /tmp
+		git clone https://aur.archlinux.org/trizen.git >>$SALIDA 2>&1 
+		cd trizen
+		makepkg -si >>$SALIDA 2>&1 
+		exit
+	' | su - sysop
+	
+	usedel -r sysop >>$SALIDA 2>&1 
+
+	exit
+" | arch-chroot /mnt
+
+echo -e "\n*******************************************************************************************************"
+echo -e "************************************** INSTALLED ******************************************************"
+echo -e "*******************************************************************************************************"
