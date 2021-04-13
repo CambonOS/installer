282e1be1eeb6712e7c7dc4c867a882a89b4c5f4f
CambonOS <82413153+CambonOS@users.noreply.github.com>
2021-04-13 13:58:00 +0200

    Update cmd.sh

--- cmd.sh
+++ cmd.sh
@@ -5,14 +5,14 @@ cd /tmp
 git clone https://github/CambonOS/Scripts.git
 cd Scripts
 
-cp CambonISO.sh /usr/bin/camboniso
+cp ./CambonISO.sh /usr/bin/camboniso
 chown root:root /usr/bin/camboniso
 chmod 755 /usr/bin/camboniso
 
-cp Actualizar.sh /usr/bin/actualizar
+cp ./Actualizar.sh /usr/bin/actualizar
 chown root:root /usr/bin/actualizar
 chmod 755 /usr/bin/actualizar
 
-cp cmd.sh /usr/bin/actualizarcmd
-chown root:root /usr/bin/actualizar
-chmod 755 /usr/bin/actualizar
+cp ./cmd.sh /usr/bin/actualizarcmd
+chown root:root /usr/bin/actualizarcmd
+chmod 755 /usr/bin/actualizarcmd
