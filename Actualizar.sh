2079bfd6deeb7c153458eab957b45888f6fe30e1
CambonOS <82413153+CambonOS@users.noreply.github.com>
2021-04-13 13:47:32 +0200

    Update Actualizar.sh

--- Actualizar.sh
+++ Actualizar.sh
@@ -1,20 +1,29 @@
 #!/bin/bash
 
-# COLORES
-NOCOLOR='\033[0m'
-BLUE='\033[1;34m'
-RED='\033[1;31m'
-GREEN='\033[0;32m'
+ERROR () {
+  echo -e "${\033[1;31m} ERROR ${\033[0m}"
+}
 
-# PROGRAMA
-sudo clear
-echo -e "${BLUE}******* ACTUALIZAR SISTEMA *******${NOCOLOR}"
-echo -e "${BLUE}\n>>Actualizando paquetes${NOCOLOR}"
-(trizen -Syyu --noconfirm >/dev/null && echo -e "${GREEN}DONE${NOCOLOR}") || echo -e "${RED}ERROR${NOCOLOR}"
-echo -e "${BLUE}\n>>Listando paquetes guerfanos...${NOCOLOR}"
+DONE () {
+  echo -e "${\033[0;32m} ERROR ${\033[0m}"
+}
+
+clear
+echo -e "******* ACTUALIZAR SISTEMA *******"
+
+echo -e "\n>>Actualizando paquetes\c"
+trizen -Syyu --noconfirm >/dev/null || ERROR
+DONE
+
+echo -e "\n>>Listando paquetes guerfanos...\c"
 trizen -Qqdt
-echo -e "${BLUE}\n>>Desistalando paquetes guerfanos${NOCOLOR}"
-trizen -Rns $(trizen -Qqdt) --noconfirm >/dev/null 2>&1; echo -e "${GREEN}DONE${NOCOLOR}"
-echo -e "${BLUE}\n>>Actualizando GRUB${NOCOLOR}"
-(sudo grub-mkconfig -o /boot/grub/grub.cfg >/dev/null 2>&1 && echo -e "${GREEN}DONE${NOCOLOR}") || echo -e "${RED}ERROR${NOCOLOR}"
-echo -e "${GREEN}******* SISTEMA ACTUALIZADO *******${NOCOLOR}"
+
+echo -e "\n>>Desistalando paquetes guerfanos\c"
+trizen -Rns $(trizen -Qqdt) --noconfirm >/dev/null 2>&1 || ERROR
+DONE
+
+echo -e "\n>>Actualizando GRUB\c"
+sudo grub-mkconfig -o /boot/grub/grub.cfg >/dev/null 2>&1 || ERROR
+DONE
+
+echo -e "******* SISTEMA ACTUALIZADO *******"
