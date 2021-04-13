a0af10cbc33825395d6359ab1ee5f1aead9feb87
CambonOS <82413153+CambonOS@users.noreply.github.com>
2021-04-13 21:53:11 +0200

    Update camboniso.sh

--- camboniso.sh
+++ camboniso.sh
@@ -31,8 +31,7 @@ cd /tmp && git clone https://github.com/CambonOS/Scripts.git && DONE || ERROR
 echo -e "\n>>Creando ficheros de configuracion de la ISO\c"
 mkdir /ISO && cp -r /usr/share/archiso/configs/releng /ISO/porfile || ERROR
 cp /tmp/Scripts/camboninstall.sh /ISO/porfile/airootfs/usr/local/bin/camboninstall || ERROR
-chown root:root /ISO/porfile/airootfs/usr/local/bin/camboninstall
-chmod 755 /ISO/porfile/airootfs/usr/local/bin/camboninstall
+echo -e "chown root:root /ISO/porfile/airootfs/usr/local/bin/camboninstall\nchmod 755 /ISO/porfile/airootfs/usr/local/bin/camboninstall" >/ISO/porfile/airootfs/root/.zshrc
 mv /tmp/Scripts/iso/paquetes /ISO/porfile/packages.x86_64 || ERROR
 echo -e "camboniso" >/ISO/porfile/airootfs/etc/hostname
 echo -e "LANG=es_ES.UTF-8\nLANGUAGE=es_ES.UTF-8\nLC_ALL=en_US.UTF-8" >/ISO/porfile/airootfs/etc/locale.conf
