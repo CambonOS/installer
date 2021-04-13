94d1f6e4a1baacebabe06224e48980ee86f4775c
CambonOS <82413153+CambonOS@users.noreply.github.com>
2021-04-13 10:13:48 +0200

    Create cmd.sh

---
+++ cmd.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cp CambonISO.sh /usr/bin/camboniso
+chown root:root /usr/bin/camboniso
+chmod 755 /usr/bin/camboniso
+cp Actualizar.sh /usr/bin/actualizar
+chown root:root /usr/bin/actualizar
+chmod 755 /usr/bin/actualizar
