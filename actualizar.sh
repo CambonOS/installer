e1663fa4929b560306b9d56c7465cef18f700407
CambonOS <82413153+CambonOS@users.noreply.github.com>
2021-04-14 17:35:52 +0200

    Update actualizar.sh

--- actualizar.sh
+++ actualizar.sh
@@ -6,28 +6,30 @@ GREEN='\033[1;32m'
 
 ERROR () {
   echo -e "${RED} ERROR ${NOCOLOR}"
+  exit
 }
 
 DONE () {
   echo -e "${GREEN} DONE ${NOCOLOR}"
+  sleep 1
 }
 
-clear
+sudo clear
 echo -e "******* ACTUALIZAR SISTEMA *******"
 
 echo -e "\n>>Actualizando paquetes\c"
-trizen -Syyu --noconfirm >/dev/null || ERROR
+trizen -Syyu --noconfirm >/tmp/Salida.txt 2>&1 || ERROR
 DONE
 
 echo -e "\n>>Listando paquetes guerfanos...\c"
 trizen -Qqdt
 
 echo -e "\n>>Desistalando paquetes guerfanos\c"
-trizen -Rns $(trizen -Qqdt) --noconfirm >/dev/null 2>&1 || ERROR
+trizen -Rns $(trizen -Qqdt) --noconfirm >>/tmp/Salida.txt 2>&1 || ERROR
 DONE
 
 echo -e "\n>>Actualizando GRUB\c"
-sudo grub-mkconfig -o /boot/grub/grub.cfg >/dev/null 2>&1 || ERROR
+sudo grub-mkconfig -o /boot/grub/grub.cfg >>/tmp/Salida.txt 2>&1 || ERROR
 DONE
 
 echo -e "******* SISTEMA ACTUALIZADO *******"
