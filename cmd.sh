2fa993b66916232c6b0920c724cbfaa527d62491
ManuCr19 <82413811+ManuCr19@users.noreply.github.com>
2021-04-16 15:24:01 +0200

    Update cmd.sh

--- cmd.sh
+++ cmd.sh
@@ -2,15 +2,11 @@
 if [[ $EUID -ne 0 ]]
 then 
 	echo -e "Debese ejecutar como usuario con privilejios"
-	exit
+	exit 1
 fi
 
 cd /tmp/Scripts
 
-cp ./camboniso.sh /usr/bin/camboniso
-chown root:root /usr/bin/camboniso
-chmod 755 /usr/bin/camboniso
+cp ./camboniso.sh /usr/bin/cambonos-iso
 
-cp ./actualizar.sh /usr/bin/actualizar
-chown root:root /usr/bin/actualizar
-chmod 755 /usr/bin/actualizar
+cp ./actualizar.sh /usr/bin/cambonos-update
