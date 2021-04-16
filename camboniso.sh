3d54b51b848237b81db126a646d24ab39fde9c6b
CambonOS <82413153+CambonOS@users.noreply.github.com>
2021-04-16 12:02:21 +0200

    Update camboniso.sh

--- camboniso.sh
+++ camboniso.sh
@@ -32,20 +32,11 @@ cd /tmp && git clone https://github.com/CambonOS/Scripts.git && DONE || ERROR
 
 echo -e "\n>>Creando ficheros de configuracion de la ISO\c"
 mkdir /ISO && cp -r /usr/share/archiso/configs/releng /ISO/porfile || ERROR
-rm /ISO/porfile/efiboot/loader/entries/archiso-x86_64-speech-linux.conf
 mv /tmp/Scripts/camboninstall.sh /ISO/porfile/airootfs/usr/local/bin/camboninstall || ERROR
 echo -e "chmod 777 /usr/local/bin/camboninstall" >>/ISO/porfile/airootfs/root/.zshrc
-mv /tmp/Scripts/iso/paquetes /ISO/porfile/packages.x86_64 || ERROR
 echo -e "camboniso" >/ISO/porfile/airootfs/etc/hostname
 echo -e "KEYMAP=es" >/ISO/porfile/airootfs/etc/vconsole.conf
-mv /tmp/Scripts/iso/porfile /ISO/porfile/profiledef.sh
-mv /tmp/Scripts/iso/tail /ISO/porfile/syslinux/archiso_tail.cfg
-mv /tmp/Scripts/iso/sys-linux /ISO/porfile/syslinux/archiso_sys-linux.cfg
-mv /tmp/Scripts/iso/head /ISO/porfile/syslinux/archiso_head.cfg
-mv /tmp/Scripts/iso/uefi /ISO/porfile/efiboot/loader/entries/archiso-x86_64-linux.conf
-mv /tmp/Scripts/iso/motd /ISO/porfile/airootfs/etc/motd
-mv /tmp/Scripts/iso/confpacman /ISO/porfile/airootfs/etc/pacman.conf
-mv /tmp/Scripts/image/8ItevIK1iZMeCvOEdUSyOHIVW4UouWlkk1p7GeDjFY0.png /ISO/porfile/syslinux/splash.png
+mv /tmp/Scripts/iso/* /ISO/porfile/ || ERROR
 DONE
 
 echo -e "\n>>Creando la ISO\n"
@@ -54,6 +45,5 @@ mkarchiso -v -w /ISO/work -o $RUTAD /ISO/porfile && DONE || ERROR
 echo -e "\n>>Eliminado ficheros/paquetes innecesarios\c"
 rm -rf /ISO
 pacman --noconfirm -Rns archiso >>/tmp/Salida.txt 2>&1
-chmod 777 $RUTAD/cambonos-*
 
 echo -e "\n\n${GREEN}***********DONE***********\n\n${NOCOLOR}"
