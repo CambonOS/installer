d96f557dfc4c18476f11ca02f3295234afe447bf
CambonOS <82413153+CambonOS@users.noreply.github.com>
2021-04-16 23:22:30 +0200

    Update cmd.sh

--- cmd.sh
+++ cmd.sh
@@ -3,19 +3,16 @@ NOCOLOR='\033[0m'
 RED='\033[1;31m'
 GREEN='\033[1;32m'
 
-DONE () {
-	echo -e "${GREEN} [DONE] ${NOCOLOR}" && sleep 1
-}
-
 ERROR () {
-	echo -e "${RED} [ERROR] ${NOCOLOR}" && sleep 3
+	echo -e "${RED} [ERROR] ${NOCOLOR}"
 }
 
-sudo cd /tmp/Scripts || ERROR
+sudo cd /tmp/Scripts || (ERROR ; echo 'El repositorio debe estar clonado en /tmp' ; exit)
 
 sudo cp ./camboniso.sh /usr/bin/cambonos-iso || ERROR
 sudo chmod 755 /usr/bin/cambonos-iso || ERROR
 
 sudo cp ./actualizar.sh /usr/bin/cambonos-upgrade || ERROR
 sudo chmod 755 /usr/bin/cambonos-upgrade || ERROR
-DONE
+
+echo -e "${GREEN} [DONE] ${NOCOLOR}"
