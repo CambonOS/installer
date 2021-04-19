c5bc372714281549471e27b679dcf8291b75dc2a
CambonOS <82413153+CambonOS@users.noreply.github.com>
2021-04-19 09:21:57 +0200

    Update cambonos-iso.sh

--- cambonos-iso.sh
+++ cambonos-iso.sh
@@ -33,8 +33,8 @@ cd /tmp && git clone https://github.com/CambonOS/arch-distro.git >>/tmp/Salida.t
 
 echo -e "\n>>Creando ficheros de configuracion de la ISO\c"
 mkdir /ISO && cp -r /usr/share/archiso/configs/releng /ISO/porfile || ERROR
-mv /tmp/arch-distro/cambonos-install.sh /ISO/porfile/airootfs/usr/local/bin/cambonos-install || ERROR
-echo 'chmod 777 /usr/local/bin/cambonos-install;VERDE="\033[1;32m";NOCOLOR="\033[0m";AZUL="\033[1;34m";echo -e "\n  Para instalar ${AZUL}CambonOS${NOCOLOR} ejecute el comando ${VERDE}cambonos-install${NOCOLOR}\n"' >>/ISO/porfile/airootfs/root/.zshrc
+mv /tmp/arch-distro/cambonos-installer.sh /ISO/porfile/airootfs/usr/local/bin/cambonos-installer || ERROR
+echo 'chmod 777 /usr/local/bin/cambonos-installer;VERDE="\033[1;32m";NOCOLOR="\033[0m";AZUL="\033[1;34m";echo -e "\n  Para instalar ${AZUL}CambonOS${NOCOLOR} ejecute el comando ${VERDE}cambonos-install${NOCOLOR}\n"' >>/ISO/porfile/airootfs/root/.zshrc
 echo -e "camboniso" >/ISO/porfile/airootfs/etc/hostname
 echo -e "KEYMAP=es" >/ISO/porfile/airootfs/etc/vconsole.conf
 cp -r /tmp/arch-distro/iso/* /ISO/porfile/ || ERROR
