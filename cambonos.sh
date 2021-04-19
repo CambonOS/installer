56f5de2a8dca5a9c221e76a6a1b1b8668f29dd51
CambonOS <82413153+CambonOS@users.noreply.github.com>
2021-04-19 23:20:32 +0200

    Update cambonos.sh

--- cambonos.sh
+++ cambonos.sh
@@ -18,9 +18,10 @@ case $1 in
 	upgrade)
 		case $2 in
 			-b)
-				echo -e "${BLUE}\n>>Actualizando comandos de CambonOS${NOCOLOR}"
+				echo -e "${BLUE}>>Actualizando comandos de CambonOS${NOCOLOR}"
+				sleep 2
 				cd /tmp; sudo rm -rf Arch-Distro >/tmp/Salida.txt 2>&1
-				git clone -b $3 https://github.com/CambonOS/Arch-Distro >>/tmp/Salida.txt 2>&1
+				git clone -b $3 https://github.com/CambonOS/Arch-Distro
 				cd Arch-Distro
 				sudo cp ./cambonos-iso.sh /usr/bin/cambonos-iso || ERROR
 				sudo chmod 755 /usr/bin/cambonos-iso || ERROR
@@ -29,9 +30,10 @@ case $1 in
 				DONE
 				;;
 			--branch)
-				echo -e "${BLUE}\n>>Actualizando comandos de CambonOS${NOCOLOR}"
+				echo -e "${BLUE}>>Actualizando comandos de CambonOS${NOCOLOR}"
+				sleep 2
 				cd /tmp; sudo rm -rf Arch-Distro >/tmp/Salida.txt 2>&1
-				git clone -b $3 https://github.com/CambonOS/Arch-Distro >>/tmp/Salida.txt 2>&1
+				git clone -b $3 https://github.com/CambonOS/Arch-Distro
 				cd Arch-Distro
 				sudo cp ./cambonos-iso.sh /usr/bin/cambonos-iso || ERROR
 				sudo chmod 755 /usr/bin/cambonos-iso || ERROR
@@ -40,9 +42,10 @@ case $1 in
 				DONE
 				;;
 			*)
-				echo -e "${BLUE}\n>>Actualizando comandos de CambonOS${NOCOLOR}"
+				echo -e "${BLUE}>>Actualizando comandos de CambonOS${NOCOLOR}"
+				sleep 2
 				cd /tmp; sudo rm -rf Arch-Distro >/tmp/Salida.txt 2>&1
-				git clone https://github.com/CambonOS/Arch-Distro >>/tmp/Salida.txt 2>&1
+				git clone https://github.com/CambonOS/Arch-Distro
 				cd Arch-Distro
 				sudo cp ./cambonos-iso.sh /usr/bin/cambonos-iso || ERROR
 				sudo chmod 755 /usr/bin/cambonos-iso || ERROR
@@ -50,34 +53,45 @@ case $1 in
 				sudo chmod 755 /usr/bin/cambonos || ERROR
 				DONE
 				echo -e "${BLUE}\n>>Actualizando paquetes${NOCOLOR}"
-				sleep 3
+				sleep 2
 				trizen -Syyu || ERROR
 				DONE
 				echo -e "${BLUE}\n>>Actualizando GRUB${NOCOLOR}"
-				sudo grub-mkconfig -o /boot/grub/grub.cfg >>/tmp/Salida.txt 2>&1 || ERROR
+				sleep 2
+				sudo grub-mkconfig -o /boot/grub/grub.cfg || ERROR
 				DONE
 				;;
 		esac
 		;;
 	install)
+		echo -e "${BLUE}>>Instalando paquetes${NOCOLOR}"
+		sleep 2
 		shift
 		trizen -Sy $* || ERROR
 		DONE
 		;;
 	list)
+		echo -e "${BLUE}>>Listando paquetes instalados${NOCOLOR}"
+		sleep 2
 		shift
 		trizen -Q $*
 		;;
 	search)
+		echo -e "${BLUE}>>Buscando paquetes${NOCOLOR}"
+		sleep 2
 		shift
 		trizen -Ss $*
 		;;
 	remove)
+		echo -e "${BLUE}>>Eliminando paquetes${NOCOLOR}"
+		sleep 2
 		shift
 		trizen -Rns $* || ERROR
 		DONE
 		;;
 	autoremove)
+		echo -e "${BLUE}>>Eliminando paquetes guerfanos${NOCOLOR}"
+		sleep 2
 		trizen -Rns $(trizen -Qqdt)
 		DONE
 		;;
@@ -85,24 +99,28 @@ case $1 in
 		shift
 		case $1 in
 			-b)
-				echo -e "${BLUE}\n>>Clonando repositorio CambonOS/Arch-Distro${NOCOLOR}"
+				echo -e "${BLUE}>>Clonando repositorio CambonOS/Arch-Distro${NOCOLOR}"
+				sleep 2
 				rm -rf Arch-Distro >/tmp/Salida.txt 2>&1
-				git clone -b $2 https://github.com/CambonOS/Arch-Distro.git >>/tmp/Salida.txt 2>&1 || ERROR
+				git clone -b $2 https://github.com/CambonOS/Arch-Distro.git || ERROR
 				;;
 			--branch)
-				echo -e "${BLUE}\n>>Clonando repositorio CambonOS/Arch-Distro${NOCOLOR}"
+				echo -e "${BLUE}>>Clonando repositorio CambonOS/Arch-Distro${NOCOLOR}"
+				sleep 2
 				rm -rf Arch-Distro >/tmp/Salida.txt 2>&1
-				git clone -b $2 https://github.com/CambonOS/Arch-Distro.git >>/tmp/Salida.txt 2>&1 || ERROR
+				git clone -b $2 https://github.com/CambonOS/Arch-Distro.git || ERROR
 				;;
 			*)
-				echo -e "${BLUE}\n>>Clonando repositorio CambonOS/Arch-Distro${NOCOLOR}"
+				echo -e "${BLUE}>>Clonando repositorio CambonOS/Arch-Distro${NOCOLOR}"
+				sleep 2
 				rm -rf Arch-Distro >/tmp/Salida.txt 2>&1
-				git clone https://github.com/CambonOS/Arch-Distro.git >>/tmp/Salida.txt 2>&1 || ERROR
+				git clone https://github.com/CambonOS/Arch-Distro.git || ERROR
 				;;
 		esac
 		DONE
 		;;
 	*)
 		echo -e "${RED}Opción ${BLUE}$1${RED} no reconocida.${NOCOLOR}"
+		sleep 2
 		;;
 esac
