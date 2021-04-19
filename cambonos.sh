c071fd5aa04762bd435d66191d828c5de8a9d52d
CambonOS <82413153+CambonOS@users.noreply.github.com>
2021-04-19 20:50:04 +0200

    Update cambonos.sh

--- cambonos.sh
+++ cambonos.sh
@@ -48,7 +48,11 @@ case $1 in
 	clone)
 		shift
 		case $1 in
-			-b,--branch)
+			-b)
+				rm -rf Arch-Distro >/tmp/Salida.txt 2>&1
+				git clone -b $2 https://github.com/CambonOS/Arch-Distro.git || ERROR
+				;;
+			--branch)
 				rm -rf Arch-Distro >/tmp/Salida.txt 2>&1
 				git clone -b $2 https://github.com/CambonOS/Arch-Distro.git || ERROR
 				;;
