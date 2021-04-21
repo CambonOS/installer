36b195072716a7e183fd56143211a6602217c92a
ManuCr19 <82413811+ManuCr19@users.noreply.github.com>
2021-04-21 13:56:03 +0200

    Update cambonos-install.sh

--- cambonos-install.sh
+++ cambonos-install.sh
@@ -158,7 +158,7 @@ echo -e "\n>>Activando zswap\c"
 echo "systemctl enable zramd.service" | CHROOT
 
 echo -e "\n>>Configurando sistema\c"
-echo 'cd /tmp && git clone https://github.com/CambonOS/arch-distro.git && cp -r arch-distro/etc/* /etc && cp -r arch-distro/usr/* /usr && cp arch-distro/etc/skel/.bashrc /root/.bashrc' | arch-chroot /mnt >>$SALIDA 2>&1
+echo 'cd /tmp && git clone https://github.com/CambonOS/arch-distro.git && cp -r arch-distro/etc/* /etc && cp -r arch-distro/usr/* /usr' | arch-chroot /mnt >>$SALIDA 2>&1
 echo 'cd /tmp && git clone https://github.com/CambonOS/arch-distro.git && bash arch-distro/cambonos.sh upgrade' | arch-chroot /mnt >>$SALIDA 2>&1
 echo "echo 'gsettings set org.gnome.desktop.background picture-uri "file:///usr/share/backgrounds/cambonos.jpg' | su $USER" | arch-chroot /mnt >>$SALIDA 2>&1
 echo "ln -sf /usr/share/zoneinfo/Europe/Madrid /etc/localtime && hwclock --systohc || exit 1" | CHROOT
