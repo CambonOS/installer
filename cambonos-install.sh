23c5320a645f00ccbddc5836588a502bd20394df
CambonOS <82413153+CambonOS@users.noreply.github.com>
2021-05-21 10:13:57 +0200

    Update cambonos-install.sh

--- cambonos-install.sh
+++ cambonos-install.sh
@@ -63,7 +63,7 @@ DM () {
 	if [[ $DM = false ]]
 	then
 		echo -e "\n>>Instalando display manager\c"
-		echo "echo 'trizen --noconfirm -Sy xorg-server lightdm lightdm-settings numlockx' | su $USER && systemctl enable lightdm.service" | ARCH && DONE || ERROR
+		echo "echo 'trizen --noconfirm -Sy xorg-server lightdm lightdm-settings numlockx || exit 1' | su $USER && systemctl enable lightdm.service" | ARCH && DONE || ERROR
 		cp -r arch-distro/configs/lightdm/* /mnt
 		cp -r share/* /mnt/usr/share
 		DM='true'
