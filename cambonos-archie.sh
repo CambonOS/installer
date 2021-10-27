a1a21b964d7734517dcef6f81926c38be56567b4
CambonOS <82413153+CambonOS@users.noreply.github.com>
2021-10-27 13:15:57 +0200

    Update cambonos-archie.sh

--- cambonos-archie.sh
+++ cambonos-archie.sh
@@ -157,7 +157,7 @@ CONFIG () {
 	chmod 775 /mnt/usr/bin/cambonos* && \
 	chmod 755 /media && \
 	echo "ln -sf /usr/share/zoneinfo/Europe/Madrid /etc/localtime && hwclock --systohc" | ARCH && \
-	echo "userdel -r $USER && useradd -m -s /bin/bash -g sudo -G lp,rfkill,wheel $USER && (echo -e '$PASS\n$PASS1' | passwd $USER)" | ARCH && \
+	echo "userdel -r $USER && useradd -m -s /bin/bash -g sudo -G lp,rfkill,wheel,sys $USER && (echo -e '$PASS\n$PASS1' | passwd $USER)" | ARCH && \
 	echo "locale-gen" | ARCH && \
 	echo "cambonos-upgrade" | ARCH && DONE || ERROR
 }
