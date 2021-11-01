d5694679602cf0739aa32707c7709cddbafb1412
CambonOS <82413153+CambonOS@users.noreply.github.com>
2021-11-01 13:13:39 +0100

    Update cambonos-install.sh

--- cambonos-install.sh
+++ cambonos-install.sh
@@ -72,7 +72,7 @@ if [[ $ANS = s ]] || [[ $ANS = si ]] || [[ $ANS = Si ]] || [[ $ANS = S ]]
   echo -e "\n>>Bind DN (cn=admin,dc=example,dc=local): \c" && read BINDDN
   echo -e "\n>>Uri (ldap://192.168.1.5): \c" && read URI
   echo -e "\n>>Bind PW (secret): \c" && read BINDPW
-  else sleep 0
+  else LDAP=false
 fi
 
 ##Particionado
