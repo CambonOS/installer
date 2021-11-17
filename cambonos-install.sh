714253286a442ddce442b145dffab0e12d7863db
CambonOS <82413153+CambonOS@users.noreply.github.com>
2021-11-17 21:53:53 +0100

    Update cambonos-install.sh

--- cambonos-install.sh
+++ cambonos-install.sh
@@ -151,7 +151,7 @@ SUDO
 echo -e "\n\n>>Desea instalar los drivers graficos? (s/N): \c" && DG
 echo -e "\n>>Desea instalar el gestor de inicico de sesion? (s/N): \c" && Dm
 echo -e "\n>>Que entorno de encritorio desea instalar:\n\n       1-CambonOS/XFCE(Recomendado)\n\n       2-CambonOS/Qtile\n\n       3-ManuCr19/i3wm"
-echo -e "\n>>Seleccionje uno, varios separados por espacios o ninguno: \c" && read ESCRITORIO
+echo -e "\n>>Seleccione uno, varios separados por espacios o ninguno: \c" && read ESCRITORIO
 echo -e "\n>>Desea unirse a un dominio LDAP? (s/N): \c"
 read ANS
 if [[ $ANS = s ]] || [[ $ANS = si ]] || [[ $ANS = Si ]] || [[ $ANS = S ]]
