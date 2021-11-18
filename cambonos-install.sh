c7df5c0c21b0eae15443bdf09999bbe8b97ba058
CambonOS <82413153+CambonOS@users.noreply.github.com>
2021-11-19 00:07:31 +0100

    Update cambonos-install.sh

--- cambonos-install.sh
+++ cambonos-install.sh
@@ -264,7 +264,7 @@ fi
 
 ##Instalacion de utilidades adicionales
 echo -e "\n>>Instalando utilidades adicionales\c"
-echo "echo 'trizen --noconfirm -Sy neofetch zsh zsh-completions zsh-autosuggestions zsh-syntax-highlighting zsh-sudo-git zsh-theme-powerlevel10k ttf-font-awesome xdg-user-dirs zramd || exit 1' | su $USER || exit 1" | ARCH
+echo "echo 'trizen --noconfirm -Sy neofetch zsh zsh-completions zsh-autosuggestions zsh-syntax-highlighting zsh-sudo-git zsh-theme-powerlevel10k ttf-meslo-nerd-font-powerlevel10k xdg-user-dirs zramd || exit 1' | su $USER || exit 1" | ARCH
 echo "systemctl enable zramd.service || exit 1" | ARCH && DONE || ERROR
 if [[ $GPU = vmware ]]
 then echo "echo 'trizen --noconfirm -Sy virtualbox-guest-utils || exit 1' | su $USER || exit 1" | ARCH && echo "systemctl enable vboxservice.service" | ARCH
