e5d5469ae8be5dd996ba03c88a8981eb2d8c068c
Cambon18 <martincambon18@gmail.com>
2023-04-15 02:13:35 +0200

    Cambio tamaño dialog

--- dialog.sh
+++ dialog.sh
@@ -1,37 +1,34 @@
 #!/bin/bash
 
-# Definicion variables
-export DIALOG_COLOR="green"
-
 while true
 do
 	# Ventana de entrada de nombre del equipo
-	NOMBRE=$(dialog --stdout --title "CambonOS Installer" --inputbox "Nombre del equipo:" 20 80)
+	NOMBRE=$(dialog --stdout --title "CambonOS Installer" --inputbox "Nombre del equipo:" 10 80)
 	
 	# Ventana de entrada de nombre para el nuevo usuario
-	USERNAME=$(dialog --stdout --title "CambonOS Installer" --inputbox "Nombre para el nuevo usuario:" 20 80)
+	USERNAME=$(dialog --stdout --title "CambonOS Installer" --inputbox "Nombre para el nuevo usuario:" 10 80)
 	USER=$(echo $USERNAME | awk '{print tolower($0)}')
 	
 	# Función para solicitar contraseña del usuario
 	function SUDO {
-	    PASS=$(dialog --stdout --title "CambonOS Installer" --passwordbox "Contraseña del usuario:" 20 80)
-	    PASS1=$(dialog --stdout --title "CambonOS Installer" --passwordbox "Repetir contraseña:" 20 80)
+	    PASS=$(dialog --stdout --title "CambonOS Installer" --passwordbox "Contraseña del usuario:" 10 80)
+	    PASS1=$(dialog --stdout --title "CambonOS Installer" --passwordbox "Repetir contraseña:" 10 80)
 	    if [[ $PASS != $PASS1 ]]
 	    then
-	        dialog --title "CambonOS Installer" --msgbox "Las contraseñas no coinciden. Inténtelo de nuevo." 20 80
+	        dialog --title "CambonOS Installer" --msgbox "Las contraseñas no coinciden. Inténtelo de nuevo." 10 80
 	        SUDO
 	    fi
 	}
 	SUDO
 	
 	# Ventana de selección de instalación de controladores gráficos
-	DG=$(dialog --stdout --title "CambonOS Installer" --yesno "Desea instalar los drivers gráficos?" 20 80 && echo "Si" || echo "No")
+	DG=$(dialog --stdout --title "CambonOS Installer" --yesno "Desea instalar los drivers gráficos?" 10 80 && echo "Si" || echo "No")
 	
 	# Ventana de selección de instalación de servidor SSH
-	SSH=$(dialog --stdout --title "CambonOS Installer" --yesno "Desea instalar servidor SSH?" 20 80 && echo "Si" || echo "No")
+	SSH=$(dialog --stdout --title "CambonOS Installer" --yesno "Desea instalar servidor SSH?" 10 80 && echo "Si" || echo "No")
 	
 	# Ventana de selección de actualizacion automatica
-	UPGRADE=$(dialog --stdout --title "CambonOS Installer" --yesno "Desea que los paquetes del sistema se actualicen automáticamente??" 20 80 && echo "Si" || echo "No")
+	UPGRADE=$(dialog --stdout --title "CambonOS Installer" --yesno "Desea que los paquetes del sistema se actualicen automáticamente??" 10 80 && echo "Si" || echo "No")
 	
 	# Ventana de selección de entorno de escritorio
 	ESCRITORIO=$(dialog --stdout --title "CambonOS Installer" --menu "Qué entorno de escritorio desea instalar?" 20 80 15 \
