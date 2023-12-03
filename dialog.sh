950b27f2104318243529954cf081ee22477229ca
ManuCr19 <82413811+ManuCr19@users.noreply.github.com>
2023-12-03 17:01:10 +0100

    Update dialog.sh

--- dialog.sh
+++ dialog.sh
@@ -7,5 +7,5 @@ pip install git+https://github.com/manucr19/py_lib.git
 python3 installer/tui.py
 
 # Monitorizacion del script de instalación
-echo "0" >/tmp/PRG
-(while [[ $(cat /tmp/PRG) -ne 100 ]]; do sleep 1; cat /tmp/PRG; done) | dialog --title " CambonOS Installer " --gauge "Instalando..." 7 80 0
+#echo "0" >/tmp/PRG
+#(while [[ $(cat /tmp/PRG) -ne 100 ]]; do sleep 1; cat /tmp/PRG; done) | dialog --title " CambonOS Installer " --gauge "Instalando..." 7 80 0
