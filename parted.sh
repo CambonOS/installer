624e02a959b35d466da0756935aed57d8f6a6024
ManuCr19 <82413811+ManuCr19@users.noreply.github.com>
2023-12-03 15:57:01 +0100

    parted.sh to installer_tui.py

--- parted.sh
+++ parted.sh
@@ -7,27 +7,15 @@ STOP () {
 
 ##Particionado
 SALIDA='/tmp/particionado.log'
-clear && cat /etc/motd
 ls /sys/firmware/efi/efivars >/dev/null 2>&1 && GRUB='uefi' || GRUB='bios'
+$DISCO = $1
+$PART = $2
+
 case $GRUB in
 	uefi)
-		echo '
-Bienvenido al instalador oficial de CambonOS!!!
-
-Para continuar su intalacion escoga entre:
-
-  1-Instalacion borrando todo el disco
-  2-Instalar sistema en el espacio libre al final del disco
-  3-Cancelar'
-		echo -e "\n(1,2,3): \c" && read PART
-		if [[ $PART != 1 ]] && [[ $PART != 2 ]]
-		then exit
-		fi
-		echo -e "\n>>Listando discos\n" && lsblk -o NAME,SIZE,VENDOR,MODEL -d
-		echo -e "\n>>En que disco desea instalar el sistema (sda,nvme0n1,...): \c" && read DISCO
 		(echo $DISCO | grep nvme >>$SALIDA 2>&1) && DISCOP=$DISCO$(echo p) || DISCOP=$DISCO
 		N=1 && LIBRE=0
-		if [[ $PART = 2 ]]
+		if [[ $PART = "end-space"]]
 		then
 			fdisk -l /dev/$DISCO | grep gpt >>$SALIDA 2>&1 && TD=gpt || TD=mbr
 			case $TD in
@@ -45,12 +33,6 @@ Para continuar su intalacion escoga entre:
 				;;
 			esac
 		else 
-			echo -e "\n>>Se eliminaran ${RED}todos los datos del disco${NOCOLOR}. Desea continuar? (s/N): \c"
-			read ANS
-			if [[ $ANS = s ]] || [[ $ANS = si ]] || [[ $ANS = Si ]] || [[ $ANS = S ]]
-			then sleep 0
-			else exit
-			fi
 			echo -e "\n>>Particionando disco...\c"
 			(echo -e "g\nn\n\n\n+512M\nn\n\n\n+30G\nn\n\n\n\nw\n" | fdisk -w always /dev/$DISCO >>$SALIDA 2>&1) || STOP
 		fi
@@ -61,24 +43,9 @@ Para continuar su intalacion escoga entre:
 		mkdir /mnt/boot >>$SALIDA 2>&1 || STOP
 		mount /dev/$DISCOP$N /mnt/boot >>$SALIDA 2>&1 || STOP && N=$(($N+2))
 		mkdir /mnt/home >>$SALIDA 2>&1 || STOP
-		mount /dev/$DISCOP$N /mnt/home >>$SALIDA 2>&1 || STOP
+		mount /dev/$DISCOP$N /mnt/home >>$SALIDA 2>&1 && DONE || STOP
 		;;
 	bios)
-		echo -e "\nBienvenido al instalador oficial de CambonOS!!!\n\n>>Con el istalador arrancado en BIOS solo se puede instalar borrando todo el disco.\n\n>>Quiere continuar? (s/N): \c"
-		read ANS
-		if [[ $ANS = s ]] || [[ $ANS = si ]] || [[ $ANS = Si ]] || [[ $ANS = S ]]
-		then
-			echo -e "\n>>Listando discos\n" && lsblk -o NAME,SIZE,VENDOR,MODEL -d
-			echo -e "\n>>En que disco desea instalar el sistema (sda,nvme0n1,...): \c" && read DISCO
-			echo -e "\n>>Se eliminaran ${RED}todos los datos del disco${NOCOLOR}. Desea continuar? (s/N): \c"
-			read ANS
-			if [[ $ANS = s ]] || [[ $ANS = si ]] || [[ $ANS = Si ]] || [[ $ANS = S ]]
-			then sleep 0
-			else exit
-			fi
-		else 
-			exit
-		fi
 		echo -e "\n>>Particionando disco...\c"
 		(echo $DISCO | grep nvme >>$SALIDA 2>&1) && DISCOP=$DISCO$(echo p) || DISCOP=$DISCO
 		(echo -e "o\nn\n\n\n\n+30G\nn\n\n\n\n\nw\n" | fdisk -w always /dev/$DISCO >>$SALIDA 2>&1) || STOP && N=1
@@ -89,5 +56,3 @@ Para continuar su intalacion escoga entre:
 		mount /dev/$DISCOP$N /mnt/home >>$SALIDA 2>&1
 		;;
 esac
-
-echo "$DISCO" >/tmp/disco
