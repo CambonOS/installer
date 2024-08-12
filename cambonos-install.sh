81378c0ee430871aa6b3dd30e273a256e13a1dca
Cambon18 <martincambon18@gmail.com>
2024-08-12 21:16:14 +0200

    Reparar cpupower

--- cambonos-install.sh
+++ cambonos-install.sh
@@ -61,7 +61,7 @@ echo "35" >/tmp/PRG
 echo "37" >/tmp/PRG
 
 # Instalacion paquetes basicos
-echo "pacman --noconfirm -Sy lsb-release tree htop xclip micro vim man man-db man-pages man-pages-es bash-completion networkmanager ntp systemd-resolvconf $CPU git wget base-devel sudo ntfs-3g dosfstools exfat-utils cpupower || exit 1" | ARCH || STOP
+echo "pacman --noconfirm -Sy lsb-release tree htop xclip micro vim man man-db man-pages man-pages-es bash-completion networkmanager ntp systemd-resolvconf $CPU git wget base-devel sudo ntfs-3g dosfstools exfat-utils cpupower rsync || exit 1" | ARCH || STOP
 echo 'systemctl enable cpupower.service || exit 1' | ARCH
 echo "45" >/tmp/PRG
 
