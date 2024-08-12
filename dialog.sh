83148bc9cca623eafd510ad597493935317710aa
Cambon18 <martincambon18@gmail.com>
2024-08-12 23:52:37 +0200

    Correcion fallo contraseña

--- dialog.sh
+++ dialog.sh
@@ -9,38 +9,16 @@ do
 	NOMBRE=$(dialog --stdout --title " CambonOS Installer " --inputbox "\nNombre del equipo:" 10 80)
 	NOMBRE=$(echo $NOMBRE | awk '{print tolower($0)}')
 	
-	
-	# Función para solicitar contraseña del usuario
-	function is_secure_password {
-	    local password="$1"
-	    local has_uppercase=$(echo "$password" | grep -c "[A-Z]")
-	    local has_lowercase=$(echo "$password" | grep -c "[a-z]")
-	    local has_number=$(echo "$password" | grep -c "[0-9]")
-	    local has_special=$(echo "$password" | grep -c "[!@#\$%\^&\*\-\+\=\.]")
-	    if [[ $has_uppercase -gt 0 && $has_lowercase -gt 0 && $has_number -gt 0 && $has_special -gt 0 && ${#password} -ge 12 ]]
-	    then
-	        return 0
-	    else
-	        return 1
-	    fi
-	}
-	
-	function SUDO {
-            RST=$1
-	    PASS=$(dialog --stdout --title " CambonOS Installer " --passwordbox "\nContraseña del usuario:" 10 80)
-	    PASS1=$(dialog --stdout --title " CambonOS Installer " --passwordbox "\nRepetir contraseña:" 10 80)
-	    if [[ $PASS != $PASS1 ]]
-	    then
-	        dialog --title " CambonOS Installer " --msgbox "\nLas contraseñas no coinciden. Inténtelo de nuevo." 7 80
-	        SUDO
-            else
-		declare -g ${RST}="$PASS"
-            fi
-	}
-	
 	# Ventana de entrada de nombre para el nuevo usuario administrador
 	ADMINNAME=$(dialog --stdout --title " CambonOS Installer " --inputbox "\nNombre para el usuario administrador:" 10 80 "Administrador")
-	SUDO ADMINPASS
+	PASS=$(dialog --stdout --title " CambonOS Installer " --passwordbox "\nContraseña del usuario:" 10 80)
+	PASS1=$(dialog --stdout --title " CambonOS Installer " --passwordbox "\nRepetir contraseña:" 10 80)
+	while [ $PASS != $PASS1 ] 
+	do
+		dialog --title " CambonOS Installer " --msgbox "\nLas contraseñas no coinciden. Inténtelo de nuevo." 7 80
+		PASS=$(dialog --stdout --title " CambonOS Installer " --passwordbox "\nContraseña del usuario:" 10 80)
+		PASS1=$(dialog --stdout --title " CambonOS Installer " --passwordbox "\nRepetir contraseña:" 10 80)
+	done
 
 	# Ventana de selección de instalación de controladores gráficos
 	DG=$(dialog --stdout --title " CambonOS Installer " --yesno "\nDesea instalar los drivers gráficos?" 7 80 && echo "Si" || echo "No")
@@ -61,11 +39,11 @@ do
 	DISCO=$(cat /tmp/disco)
 	
 	# Ventana de confirmación opciones	
-	dialog --title " CambonOS Installer " --yesno "\nPor favor, confirme que las opciones seleccionadas son correctas:\n\nNombre del equipo: $NOMBRE\nNombre para el administrador: $ADMINNAME\nNombre para el nuevo usuario: $USERNAME\nInstalar los drivers gráficos: $DG\nInstalar servidor SSH: $SSH\nActualización automatica: $UPGRADE\nEntorno de escritorio seleccionado: $ESCRITORIO" 15 80 && break
+	dialog --title " CambonOS Installer " --yesno "\nPor favor, confirme que las opciones seleccionadas son correctas:\n\nNombre del equipo: $NOMBRE\nNombre para el administrador: $ADMINNAME\nInstalar los drivers gráficos: $DG\nInstalar servidor SSH: $SSH\nActualización automatica: $UPGRADE\nEntorno de escritorio seleccionado: $ESCRITORIO" 15 80 && break
 done
 
 # Ejecucion del script de instalación
-sh installer/cambonos-install.sh $NOMBRE $ADMINNAME $ADMINPASS $DG $SSH $UPGRADE $ESCRITORIO $DISCO >/tmp/install 2>&1 &
+sh installer/cambonos-install.sh $NOMBRE $ADMINNAME $PASS $DG $SSH $UPGRADE $ESCRITORIO $DISCO >/tmp/install 2>&1 &
 
 # Monitorizacion del script de instalación
 echo "0" >/tmp/PRG
