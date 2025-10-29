23e26f63d36aaa19b095c0b5a4ca7417d8404786
Cambon18 <82413153+Cambon18@users.noreply.github.com>
2025-10-29 09:36:46 +0100

    Update cambonos-install.sh

--- cambonos-install.sh
+++ cambonos-install.sh
@@ -24,6 +24,7 @@ SSH=$5
 UPGRADE=$6
 ESCRITORIO=$7
 DISCO=$8
+echo "1" >/tmp/PRG
 
 # Habilitar NTP
 timedatectl set-ntp true
@@ -31,15 +32,15 @@ echo "2" >/tmp/PRG
 
 # Generar lista de mirrors
 reflector -l 10 -f 5 --save /etc/pacman.d/mirrorlist
-echo "10" >/tmp/PRG
+echo "5" >/tmp/PRG
 
 # Actualizacion de las claves de Arch Linux
 pacman --noconfirm -Sy archlinux-keyring
-echo "15" >/tmp/PRG
+echo "10" >/tmp/PRG
 
 # Creacion de la raiz del sistema
 pacstrap /mnt linux linux-headers linux-firmware base || STOP
-echo "30" >/tmp/PRG
+echo "25" >/tmp/PRG
 
 # Generar fichero fstab del sistema
 dd if=/dev/zero of=/mnt/swapfile bs=1M count=4k status=progress
@@ -47,7 +48,7 @@ chmod 0600 /mnt/swapfile
 mkswap -U clear /mnt/swapfile
 swapon /mnt/swapfile
 genfstab -U /mnt >> /mnt/etc/fstab || STOP
-echo "33" >/tmp/PRG
+echo "30" >/tmp/PRG
 
 # Modificar configuraciones de root
 echo "usermod -s /bin/zsh root" | ARCH # Cambio shell
@@ -58,16 +59,16 @@ echo "35" >/tmp/PRG
 
 # Definicion de los paquetes microcode CPU
 (grep 'Intel' /proc/cpuinfo >/dev/null && CPU='intel-ucode') || (grep 'AMD' /proc/cpuinfo >/dev/null && CPU='amd-ucode') || CPU='amd-ucode intel-ucode'
-echo "37" >/tmp/PRG
+echo "40" >/tmp/PRG
 
 # Instalacion paquetes basicos
 echo "pacman --noconfirm -Sy lsb-release tree htop xclip micro vim man man-db man-pages man-pages-es bash-completion networkmanager ntp systemd-resolvconf $CPU git wget base-devel sudo ntfs-3g dosfstools exfat-utils cpupower rsync plymouth accountsservice || exit 1" | ARCH || STOP
 echo 'systemctl enable cpupower.service ; systemctl enable accounts-daemon.service || exit 1' | ARCH
-echo "45" >/tmp/PRG
+echo "50" >/tmp/PRG
 
 # Habilitar repositorios multilib
 echo -e "\n[multilib]\nInclude = /etc/pacman.d/mirrorlist" >>/mnt/etc/pacman.conf
-echo "47" >/tmp/PRG
+echo "52" >/tmp/PRG
 
 # Instalacion drivers graficos
 if [[ $DG =~ ^([sS]|si|Si)$ ]]; then
@@ -101,7 +102,7 @@ if [[ $DG =~ ^([sS]|si|Si)$ ]]; then
             ;;
     esac
 fi
-echo "50" >/tmp/PRG
+echo "60" >/tmp/PRG
 
 # Instalacion GRUB
 ls /sys/firmware/efi/efivars >/dev/null 2>&1 && GRUB='uefi' || GRUB='bios'
@@ -113,7 +114,7 @@ case $GRUB in
 		echo "pacman --noconfirm -Sy grub os-prober grub-theme-vimix && grub-install --target=i386-pc /dev/$DISCO || exit 1" | ARCH || STOP
 		;;
 esac
-echo "55" >/tmp/PRG
+echo "65" >/tmp/PRG
 
 # Configuraciones de Red
 cp /etc/NetworkManager/system-connections/* /mnt/etc/NetworkManager/system-connections
@@ -123,14 +124,14 @@ echo -e "127.0.0.1	localhost\n::1		localhost\n127.0.1.1	$NOMBRE" >/mnt/etc/hosts
 sed -i 's/^#MulticastDNS=yes/MulticastDNS=no/' /mnt/etc/systemd/resolved.conf
 sed -i 's/^use-ipv6=yes/use-ipv6=no/' /mnt/etc/avahi/avahi-daemon.conf
 echo 'systemctl enable NetworkManager.service ; systemctl enable ntpd.service ; systemctl enable systemd-resolved.service ; systemctl enable avahi-daemon.service ; systemctl enable systemd-homed.service || exit 1' | ARCH
-echo "60" >/tmp/PRG
+echo "66" >/tmp/PRG
 
 # Instalacion de yay
 echo "groupadd -g 777 updates" | ARCH
 echo "useradd -m -d /home/.updates -g updates -u 777 updates && passwd --lock updates || exit 1" | ARCH
 echo -e "\n%updates ALL=(ALL) NOPASSWD: ALL" >> /mnt/etc/sudoers
 echo "echo 'cd /tmp && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg --noconfirm -si || exit 1' | su updates || exit 1" | ARCH
-echo "65" >/tmp/PRG
+echo "68" >/tmp/PRG
 
 # Instalacion de utilidades adicionales
 echo "echo 'yay --noconfirm -Sy neofetch zsh zsh-completions zsh-autosuggestions zsh-syntax-highlighting zsh-theme-powerlevel10k-bin-git ttf-meslo-nerd-font-powerlevel10k xdg-user-dirs libpwquality || exit 1' | su updates || exit 1" | ARCH
