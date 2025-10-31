06b9eeffd74332ea271cf2042577798b73f37e92
Cambon18 <82413153+Cambon18@users.noreply.github.com>
2025-10-31 08:39:01 +0100

    Update cambonos-install.sh

--- cambonos-install.sh
+++ cambonos-install.sh
@@ -22,23 +22,22 @@ ADMINPASS=$3
 ESCRITORIO=$4
 SSH=$5
 DISCO=$6
-echo "1" >/tmp/PRG
 
 # Habilitar NTP
 timedatectl set-ntp true
-echo "2" >/tmp/PRG
+echo "1" >/tmp/PRG
 
 # Generar lista de mirrors
 reflector -l 10 -f 5 --save /etc/pacman.d/mirrorlist
-echo "5" >/tmp/PRG
+echo "3" >/tmp/PRG
 
 # Actualizacion de las claves de Arch Linux
 pacman --noconfirm -Sy archlinux-keyring
-echo "7" >/tmp/PRG
+echo "4" >/tmp/PRG
 
 # Creacion de la raiz del sistema
 pacstrap /mnt linux linux-headers linux-firmware base || STOP
-echo "15" >/tmp/PRG
+echo "7" >/tmp/PRG
 
 # Generar fichero fstab del sistema
 dd if=/dev/zero of=/mnt/swapfile bs=1M count=4k status=progress
@@ -46,20 +45,17 @@ chmod 0600 /mnt/swapfile
 mkswap -U clear /mnt/swapfile
 swapon /mnt/swapfile
 genfstab -U /mnt >> /mnt/etc/fstab || STOP
-echo "20" >/tmp/PRG
+echo "8" >/tmp/PRG
 
 # Modificar configuraciones de root
 echo "usermod -s /bin/zsh root" | ARCH # Cambio shell
 cp -rv installer/cambonos-fs/etc/skel/.config /mnt/root # Carpeta .config del skel
 cp -v installer/cambonos-fs/etc/skel/.* /mnt/root/ # Ficheros del skel
 echo "passwd --lock root" | ARCH
-echo "24" >/tmp/PRG
-
-# Definicion de los paquetes microcode CPU
-(grep 'Intel' /proc/cpuinfo >/dev/null && CPU='intel-ucode') || (grep 'AMD' /proc/cpuinfo >/dev/null && CPU='amd-ucode') || CPU='amd-ucode intel-ucode'
-echo "25" >/tmp/PRG
+echo "9" >/tmp/PRG
 
 # Instalacion paquetes basicos
+(grep 'Intel' /proc/cpuinfo >/dev/null && CPU='intel-ucode') || (grep 'AMD' /proc/cpuinfo >/dev/null && CPU='amd-ucode') || CPU='amd-ucode intel-ucode'
 packages="lsb-release tree htop xclip micro vim man man-db man-pages man-pages-es bash-completion networkmanager ntp systemd-resolvconf $CPU git wget base-devel sudo ntfs-3g dosfstools exfat-utils cpupower rsync plymouth accountsservice"
 read -r -a pkg_array <<< "$packages"
 n=${#pkg_array[@]}
@@ -67,14 +63,14 @@ i=0
 for pkg in "${pkg_array[@]}"; do
     i=$((i + 1))
     echo "pacman --noconfirm -Sy $pkg" | ARCH || STOP
-    progress=$((25 + (i * 20 / n)))
+    progress=$((9 + ($i * 9 / $n)))
     echo "$progress" > /tmp/PRG
 done
 echo 'systemctl enable cpupower.service ; systemctl enable accounts-daemon.service || exit 1' | ARCH
 
 # Habilitar repositorios multilib
 echo -e "\n[multilib]\nInclude = /etc/pacman.d/mirrorlist" >>/mnt/etc/pacman.conf
-echo "47" >/tmp/PRG
+echo "19" >/tmp/PRG
 
 # Instalacion drivers graficos
 # Detectar GPU
@@ -103,7 +99,7 @@ i=0
 for pkg in "${pkg_array[@]}"; do
     i=$((i + 1))
     echo "pacman --noconfirm -Sy $pkg" | ARCH || STOP
-    progress=$((47 + (i * 10 / n)))
+    progress=$((19 + ($i * 5 / $n)))
     echo "$progress" > /tmp/PRG
 done
 
@@ -117,7 +113,7 @@ case $GRUB in
 		echo "pacman --noconfirm -Sy grub os-prober grub-theme-vimix && grub-install --target=i386-pc /dev/$DISCO || exit 1" | ARCH || STOP
 		;;
 esac
-echo "62" >/tmp/PRG
+echo "27" >/tmp/PRG
 
 # Configuraciones de Red
 cp /etc/NetworkManager/system-connections/* /mnt/etc/NetworkManager/system-connections
@@ -127,14 +123,14 @@ echo -e "127.0.0.1	localhost\n::1		localhost\n127.0.1.1	$NOMBRE" >/mnt/etc/hosts
 sed -i 's/^#MulticastDNS=yes/MulticastDNS=no/' /mnt/etc/systemd/resolved.conf
 sed -i 's/^use-ipv6=yes/use-ipv6=no/' /mnt/etc/avahi/avahi-daemon.conf
 echo 'systemctl enable NetworkManager.service ; systemctl enable ntpd.service ; systemctl enable systemd-resolved.service ; systemctl enable avahi-daemon.service ; systemctl enable systemd-homed.service || exit 1' | ARCH
-echo "63" >/tmp/PRG
+echo "31" >/tmp/PRG
 
 # Instalacion de yay
 echo "groupadd -g 777 updates" | ARCH
 echo "useradd -m -d /home/.updates -g updates -u 777 updates && passwd --lock updates || exit 1" | ARCH
 echo -e "\n%updates ALL=(ALL) NOPASSWD: ALL" >> /mnt/etc/sudoers
 echo "echo 'cd /tmp && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg --noconfirm -si || exit 1' | su updates || exit 1" | ARCH
-echo "65" >/tmp/PRG
+echo "33" >/tmp/PRG
 
 # Instalacion de utilidades adicionales
 if [[ $GPU = nvidia-hybrid ]]
@@ -149,12 +145,12 @@ i=0
 for pkg in "${pkg_array[@]}"; do
     i=$((i + 1))
     echo "yay --noconfirm -Sy $pkg" | ARCH || STOP
-    progress=$((65 + (i * 5 / n)))
+    progress=$((33 + ($i * 8 / $n)))
     echo "$progress" > /tmp/PRG
 done
 
 # Instalacion XFCE
-(sleep 2; while [[ $(cat /mnt/tmp/PRG) -ne 85 ]]; do cp /mnt/tmp/PRG /tmp/PRG; sleep 1; done) &
+(sleep 2; while [[ $(cat /mnt/tmp/PRG) -ne 88 ]]; do cp /mnt/tmp/PRG /tmp/PRG; sleep 1; done) &
 case "$ESCRITORIO" in
     1)
         # Instalación XFCE
@@ -169,7 +165,6 @@ case "$ESCRITORIO" in
 		rm -rf /mnt/qtile
         ;;
 esac
-echo "85" >/tmp/PRG
 
 # Configuraciones CambonOS
 cp -rv installer/cambonos-fs/* /mnt
@@ -189,20 +184,21 @@ then
 	echo "pacman --noconfirm -Sy openssh && sed -i s/#X11Forwarding\ no/X11Forwarding\ yes/ /etc/ssh/sshd_config; systemctl enable sshd.service || exit 1" | ARCH
 	echo -e "*filter\n:INPUT DROP [0:0]\n:FORWARD DROP [0:0]\n:OUTPUT ACCEPT [0:0]\n-A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT\n-A INPUT -s 127.0.0.1 -j ACCEPT\n-A INPUT -p tcp --dport 22 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT\n-A INPUT -p udp --dport 5353 -j ACCEPT\nCOMMIT" >/mnt/etc/iptables/iptables.rules
 fi
+echo "91" >/tmp/PRG
 
 # Configuracion hora
 echo "ln -sf /usr/share/zoneinfo/Europe/Madrid /etc/localtime && hwclock --systohc" | ARCH
-echo "93" >/tmp/PRG
+echo "92" >/tmp/PRG
 
 # Creacion usuario
 echo "useradd -m -c $ADMINNAME -s /bin/zsh -g users -G wheel,rfkill,sys,lp $ADMINUSER && (echo -e '$ADMINPASS\n$ADMINPASS' | passwd $ADMINUSER)" | ARCH
-echo "94" >/tmp/PRG
+echo "93" >/tmp/PRG
 
 # Configuracion cambonos-upgrade
 echo "chown updates:wheel /usr/bin/cambonos-upgrade; chmod 750 /usr/bin/cambonos-upgrade" | ARCH
 echo "chsh -s /usr/bin/nologin updates" | ARCH
 echo "systemctl enable cambonos-upgrade.timer || exit 1" | ARCH
-echo "96" >/tmp/PRG
+echo "94" >/tmp/PRG
 
 # Generacion locales
 echo "locale-gen" | ARCH
