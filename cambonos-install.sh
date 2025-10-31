7cd39f65968005a2107abd53a3e580c07d56a845
Cambon18 <82413153+Cambon18@users.noreply.github.com>
2025-10-31 08:58:41 +0100

    Update cambonos-install.sh

--- cambonos-install.sh
+++ cambonos-install.sh
@@ -144,7 +144,7 @@ n=${#pkg_array[@]}
 i=0
 for pkg in "${pkg_array[@]}"; do
     i=$((i + 1))
-    echo "yay --noconfirm -Sy $pkg" | ARCH || STOP
+    echo "yay --noconfirm -Sy $pkg" | ARCH
     progress=$((33 + ($i * 8 / $n)))
     echo "$progress" > /tmp/PRG
 done
